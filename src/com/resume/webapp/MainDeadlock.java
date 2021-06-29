20c752a0c60af3d3a8307c513a906cb1501a9626
Сергей Антонов <sergkurs2006@mail.ru>
2021-06-29 18:03:05 +0300

    ДЗ №11. Исправление замечаний №2.

--- src/com/resume/webapp/MainDeadlock.java
+++ src/com/resume/webapp/MainDeadlock.java
@@ -5,20 +5,18 @@ public class MainDeadlock {
     private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        Thread T1 = new Thread(() -> metod("Thread1", LOCK, LOCK2));
-        Thread T2 = new Thread(() -> metod("Thread2", LOCK2, LOCK));
-        T1.start();
-        T2.start();
+        new Thread(() -> metod("Thread1", LOCK, LOCK2)).start();
+        new Thread(() -> metod("Thread2", LOCK2, LOCK)).start();
     }
 
     private static void metod(String thread, Object x, Object y) {
         synchronized (x) {
-            System.out.println(thread + ": Holding lock...");
+            System.out.println(thread + ": Holding lock..."+x);
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
             }
-            System.out.println(thread + ": Waiting for lock...");
+            System.out.println(thread + ": Waiting for lock..."+y);
             synchronized (y) {
                 System.out.println("Thread: Holding lock 1 & 2...");
             }
